d6d80a64404637d858d0819df65397ccf12d7b49
Emilioglez873 <guimelemilio@gmai.com>
2024-10-21 08:54:30 -0600

    [ADD] 12. Filtering tasks

--- app.js
+++ app.js
@@ -77,10 +77,26 @@ class Root extends Component {
     <div class="todo-app">
     <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input"/>
         <div class="task-list">
-            <t t-foreach="store.tasks" t-as="task" t-key="task.id">
+            <t t-foreach="displayedTasks" t-as="task" t-key="task.id">
                 <Task task="task"/>
             </t>
         </div>
+        <div class="task-panel" t-if="store.tasks.length">
+            <div class="task-counter">
+                <t t-esc="displayedTasks.length"/>
+                <t t-if="displayedTasks.length lt store.tasks.length">
+                    / <t t-esc="store.tasks.length"/>
+                </t>
+                task(s)
+            </div>
+        <div>
+          <span t-foreach="['all', 'active', 'completed']"
+            t-as="f" t-key="f"
+            t-att-class="{active: filter.value===f}"
+            t-on-click="() => this.setFilter(f)"
+            t-esc="f"/>
+        </div>
+      </div>
     </div>`;
 
     static components = { Task };
@@ -88,7 +104,8 @@ class Root extends Component {
     setup(){
         const inputRef = useRef("add-input");
         onMounted(() => inputRef.el.focus());
-        this.store = useStore();    
+        this.store = useStore();
+        this.filter = useState({ value: "all" });
     }
 
     addTask(ev) {
@@ -98,6 +115,18 @@ class Root extends Component {
             ev.target.value = "";
         }
     }
+    get displayedTasks() {
+        const tasks = this.store.tasks;
+        switch (this.filter.value) {
+          case "active": return tasks.filter(t => !t.isCompleted);
+          case "completed": return tasks.filter(t => t.isCompleted);
+          case "all": return tasks;
+        }
+      }
+    
+      setFilter(filter) {
+        this.filter.value = filter;
+      }
     
 }
 // -------------------------------------------------------------------------
