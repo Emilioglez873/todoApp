90fae8cb4bfa066f988f3a0915997dc2bef30de1
Emilioglez873 <guimelemilio@gmai.com>
2024-10-21 08:09:30 -0600

    [ADD] 10. Using a store

--- app.js
+++ app.js
@@ -1,64 +1,102 @@
-const { Component, mount, xml, useRef, onMounted, useState } = owl;
+const { Component, mount, xml, useRef, onMounted, useState, useEnv, reactive } = owl;
 
-// Owl Components
+// -------------------------------------------------------------------------
+// Store
+// -------------------------------------------------------------------------
+function useStore() {
+    const env = useEnv();
+    return useState(env.store);
+}
+
+// -------------------------------------------------------------------------
+// TaskList
+// -------------------------------------------------------------------------
+class TaskList {
+    nextId = 1;
+    tasks = [];
+  
+    addTask(text) {
+      text = text.trim();
+      if (text) {
+        const task = {
+          id: this.nextId++,
+          text: text,
+          isCompleted: false,
+        };
+        this.tasks.push(task);
+      }
+    }
+  
+    toggleTask(task) {
+      task.isCompleted = !task.isCompleted;
+    }
+  
+    deleteTask(task) {
+      const index = this.tasks.findIndex((t) => t.id === task.id);
+      this.tasks.splice(index, 1);
+    }
+}
+  
+function createTaskStore() {
+    const taskList = new TaskList();
+    return reactive(taskList);
+}
+
+// -------------------------------------------------------------------------
+// Task Component
+// -------------------------------------------------------------------------
 class Task extends Component {
     static template = xml /* xml */`
       <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
-        <input type="checkbox" t-att-checked="props.task.isCompleted" t-on-click="toggleTask"/>
+        <input type="checkbox" t-att-checked="props.task.isCompleted" t-on-click="() => store.toggleTask(props.task)"/>
         <span><t t-esc="props.task.text"/></span>
-        <span class="delete" t-on-click="deleteTask">🗑</span>
+        <span class="delete" t-on-click="() => store.deleteTask(props.task)">🗑</span>
       </div>`;
     
-    static props = ["task", "onDelete"];
+    static props = ["task"];
 
-    deleteTask() {
-        this.props.onDelete(this.props.task);
+    setup(){
+        this.store = useStore();
     }
-    toggleTask() {
-        this.props.task.isCompleted = !this.props.task.isCompleted;
-    }
-  }
-  
+    
+}
+
+// -------------------------------------------------------------------------
+// Root Component
+// -------------------------------------------------------------------------
 class Root extends Component {
     static template = xml/* xml */ `
     <div class="todo-app">
     <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input"/>
         <div class="task-list">
-            <t t-foreach="tasks" t-as="task" t-key="task.id">
-                <Task task="task" onDelete.bind="deleteTask"/>
+            <t t-foreach="store.tasks" t-as="task" t-key="task.id">
+                <Task task="task"/>
             </t>
         </div>
     </div>`;
 
     static components = { Task };
 
-    nextId = 1;
-    tasks = useState([]);
     setup(){
         const inputRef = useRef("add-input");
-        onMounted(() => inputRef.el.focus());    
+        onMounted(() => inputRef.el.focus());
+        this.store = useStore();    
     }
 
     addTask(ev) {
         // 13 is keycode for ENTER
         if (ev.keyCode === 13) {
-            const text = ev.target.value.trim();
+            this.store.addTask(ev.target.value);
             ev.target.value = "";
-            if (text) {
-                const newTask = {
-                    id: this.nextId++,
-                    text: text,
-                    isCompleted: false,
-                };
-                this.tasks.push(newTask);
-            }
         }
     }
-    deleteTask(task) {
-        const index = this.tasks.findIndex(t => t.id === task.id);
-        this.tasks.splice(index, 1);
-    }
-
+    
 }
+// -------------------------------------------------------------------------
+// Setup
+// -------------------------------------------------------------------------
+const env = {
+    store: createTaskStore(),
+};
 
-mount(Root, document.body, {dev: true});
+mount(Root, document.body, {dev: true, env});
